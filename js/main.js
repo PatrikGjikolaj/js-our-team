c1731e457a19d4b152d4b258c50e036a7b7d43b8
PatrikGjikolaj <123626448+PatrikGjikolaj@users.noreply.github.com>
2023-05-02 16:26:56 +0200

    aggiunto stampa dom

--- js/main.js
+++ js/main.js
@@ -4,13 +4,25 @@ const team = [
     { nome: "Walter Gordon", ruolo: "Office Manager", foto: "walter-gordon-office-manager.jpg" },
     { nome: "Angela Lopez", ruolo: "Social Media Manager", foto: "angela-lopez-social-media-manager.jpg" },
     { nome: "Scott Estrada", ruolo: "Developer", foto: "scott-estrada-developer.jpg" },
-    { nome: "Barbara Ramos", ruolo: "Graphic Designer", foto: "barbara-ramos-graphic-designer.jpg" },
+    { nome: "Barbara Ramos", ruolo: "Graphic Designer", foto: "barbara-ramos-graphic-designer.jpg" }
 ]
-let utente1 = team[0] 
-console.log(team);
+
+for (const key in team) {
+    console.log(`Il membro ${key} ha le seguenti informazioni:`, team[key]);
+}
 
 for (let i = 0; i < team.length; i++) {
-    document.getElementById("nome").innerText = team[i].nome;
-    document.getElementById("ruolo").innerText = team[i].ruolo;
-    document.getElementById("foto").innerText = team[i].foto;
+    
+    const nuovoDiv = document.createElement("div");
+    const nuovoDiv1 = document.createElement("div");
+    const nuovoDiv2 = document.createElement("div");
+    nuovoDiv.innerText = team[i].nome;
+    nuovoDiv1.innerText = team[i].ruolo;
+    nuovoDiv2.innerText = team[i].foto;
+    griglia.appendChild(nuovoDiv);
+    griglia.appendChild(nuovoDiv1);
+    griglia.appendChild(nuovoDiv2);
+    nuovoDiv.classList.add("scheda", "my-3");
+    nuovoDiv1.classList.add("scheda", "my-3");
+    nuovoDiv2.classList.add("scheda", "my-3");
 }
