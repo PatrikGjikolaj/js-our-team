991f213fea5160fcffaac87295b039be638583b0
PatrikGjikolaj <123626448+PatrikGjikolaj@users.noreply.github.com>
2023-05-02 16:33:24 +0200

    esercizio completo senza personalizzazioni

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const team = [
-    { nome: "Wayne Barnett", ruolo: "Founder & CEO", foto: "wayne-barnett-founder-ceo.jpg" },
-    { nome: "Angela Caroll", ruolo: "Chief Editor", foto: "angela-caroll-chief-editor.jpg" },
-    { nome: "Walter Gordon", ruolo: "Office Manager", foto: "walter-gordon-office-manager.jpg" },
-    { nome: "Angela Lopez", ruolo: "Social Media Manager", foto: "angela-lopez-social-media-manager.jpg" },
-    { nome: "Scott Estrada", ruolo: "Developer", foto: "scott-estrada-developer.jpg" },
-    { nome: "Barbara Ramos", ruolo: "Graphic Designer", foto: "barbara-ramos-graphic-designer.jpg" }
+    { nome: "Wayne Barnett", ruolo: "Founder & CEO", foto: "./img/wayne-barnett-founder-ceo.jpg" },
+    { nome: "Angela Caroll", ruolo: "Chief Editor", foto: "./img/angela-caroll-chief-editor.jpg" },
+    { nome: "Walter Gordon", ruolo: "Office Manager", foto: "./img/walter-gordon-office-manager.jpg" },
+    { nome: "Angela Lopez", ruolo: "Social Media Manager", foto: "./img/angela-lopez-social-media-manager.jpg" },
+    { nome: "Scott Estrada", ruolo: "Developer", foto: "./img/scott-estrada-developer.jpg" },
+    { nome: "Barbara Ramos", ruolo: "Graphic Designer", foto: "./img/barbara-ramos-graphic-designer.jpg" }
 ]
 
 for (const key in team) {
@@ -15,10 +15,10 @@ for (let i = 0; i < team.length; i++) {
     
     const nuovoDiv = document.createElement("div");
     const nuovoDiv1 = document.createElement("div");
-    const nuovoDiv2 = document.createElement("div");
+    const nuovoDiv2 = document.createElement("img");
     nuovoDiv.innerText = team[i].nome;
     nuovoDiv1.innerText = team[i].ruolo;
-    nuovoDiv2.innerText = team[i].foto;
+    nuovoDiv2.src = team[i].foto;
     griglia.appendChild(nuovoDiv);
     griglia.appendChild(nuovoDiv1);
     griglia.appendChild(nuovoDiv2);
